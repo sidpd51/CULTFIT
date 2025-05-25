9d469a5159d0fc6ed30f1b7882772914637050b3
sidpd51 <sidpd510@gmail.com>
2025-05-25 18:16:17 +0530

    feat: Refactor class schedule handlers and services to integrate class instance queue processing

--- src/controllers/classSchedule.controller.ts
+++ src/controllers/classSchedule.controller.ts
@@ -1,21 +1,20 @@
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { createClassSchedule } from "../repositories/classSchedule.repository";
-import { BadRequestError, InternalServerError } from "../utils/errors/app.error";
 import { logger } from "../config/logger.config";
-import { fetchCenterClassSchedulesService } from "../services/classSchedule.service";
+import { createClassScheduleService, fetchCenterClassSchedulesService } from "../services/classSchedule.service";
+import { BadRequestError, InternalServerError } from "../utils/errors/app.error";
 
 export const createClassScheduleHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const result = await createClassSchedule(req.body);
+        const result = await createClassScheduleService(req.body);
         logger.info("Class Scheduled successfully through createClassScheduleHandler");
-
         res.status(StatusCodes.OK).json({
             message: `Class Scheduled successfully for center id: ${req.body.centerId}`,
             success: true,
             data: result
         });
     } catch (error) {
+        console.log(error);
         if (error instanceof BadRequestError) {
             logger.error(`Error in createClassScheduleHandler, ${error.message}`);
             res.status(error.statusCode).json({
--- src/services/classSchedule.service.ts
+++ src/services/classSchedule.service.ts
@@ -1,11 +1,16 @@
 import { classScheduleType } from "../dto/classSchedule.dto";
+import { classInstanceQueue } from "../jobs/queues/class-instance-queue";
+import { ClassInstanceStatus } from "../models/classInstance.model";
 import { createClassSchedule, fetchCenterClassSchedules } from "../repositories/classSchedule.repository";
 
 export const createClassScheduleService = async (payload: classScheduleType) => {
     try {
         const classSchedule = await createClassSchedule(payload);
+        await pushClassInstanceToQueue(classSchedule.id);
+
         return classSchedule;
     } catch (error) {
+        console.log(error)
         throw error;
     }
 }
@@ -16,5 +21,24 @@ export const fetchCenterClassSchedulesService = async (centerId: number) => {
         return classSchedules;
     } catch (error) {
         throw error;
+    }
+}
+
+const pushClassInstanceToQueue = async (id: number) => {
+    try {
+
+        await classInstanceQueue.add("class-instance-queue", {
+            classScheduleId: id,
+            centerId: 1,
+            classTypeId: 1,
+            date: '2025-05-29',
+            startTime: '08:45',
+            status: ClassInstanceStatus.SCHEDULED,
+        }, {
+            removeOnComplete: 1000,
+            removeOnFail: 5000,
+        });
+    } catch (error) {
+        console.log("From pushClassInstanceToQueue:", error)
     }
 }
